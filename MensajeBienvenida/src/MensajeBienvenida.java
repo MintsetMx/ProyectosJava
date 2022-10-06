1d96f2fa248d5f795e09584034666917e778d03b
MintsetMx <108957035+MintsetMx@users.noreply.github.com>
2022-10-05 19:38:40 -0500

    Add files via upload

---
+++ MensajeBienvenida/src/MensajeBienvenida.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author MrDan
+ */
+public class MensajeBienvenida {
+
+    public static void main(String[] args) {
+        
+        //Declaramos la variable de tipo String
+        String nombre ="Daniel";
+        
+        System.out.println("Bienvenido "+nombre);
+    }
+    
+}
