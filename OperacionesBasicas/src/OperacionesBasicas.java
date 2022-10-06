054819e8f77ecc6dd961e0b4cdb192de4bae68d6
MintsetMx <108957035+MintsetMx@users.noreply.github.com>
2022-10-05 19:26:18 -0500

    Add files via upload

---
+++ OperacionesBasicas/src/OperacionesBasicas.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author MrDan
+ */
+public class OperacionesBasicas {
+
+    public static void main(String[] args) {
+        
+        //Declaramos e iniciamos con una valor las variables
+        int num1 =10;
+        int num2 = 5;
+ 
+        //Realizamos las operaciones.
+        
+        //Suma
+        System.out.println("El resultado de la suma es "+(num1+num2));
+        
+        //Resta
+        System.out.println("El resultado de la resta es: "+(num1-num2));
+        
+        //Multiplicacion
+        System.out.println("El resultado de la multiplicacion es: "+(num1*num2));
+        
+        //Division
+        System.out.println("El resultado de la multiplicacion es: "+(num1/num2));
+        
+    }
+    
+}
